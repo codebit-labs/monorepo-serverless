84b1787378acaf53faaf365467ec05086f5b854f
Julio César Barrera Andrade <julio.barrera@yaydoo.com>
2020-11-20 21:25:32 -0600

    feat: eventbridge support

--- packages/hello/router.ts
+++ packages/hello/router.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from "express";
+import slsw from "serverless-webpack";
+import { EventBridge } from "aws-sdk";
 
 const router = express.Router();
 
-router.get("/", (_req: Request, res: Response) => {
-  return res.json({ Hello: "Worlds" });
+router.get("/", async (_req: Request, res: Response) => {
+  const eventBridge = new EventBridge({
+    endpoint: process.env.EVENT_BRIDGE_ENDPOINT,
+  });
+  eventBridge.putEvents({
+    Entries: [
+      {
+        Source: process.env.EVENT_BRIDGE_SOURCE,
+        DetailType: "EXAMPLE",
+        Detail: JSON.stringify({
+          action: "DISPATCH",
+        }),
+        Time: new Date(),
+      },
+    ],
+  });
+  return res.json({ Hello: "World" });
 });
+
 router.get(
   "*",
   (_req, res): Response => {
