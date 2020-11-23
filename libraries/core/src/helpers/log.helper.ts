b8bced72fca12009e53f067dc0635b68154f70a1
Julio César Barrera Andrade <julio.barrera@yaydoo.com>
2020-11-23 08:15:37 -0600

    refactor: change 1 by 2

--- libraries/core/src/helpers/log.helper.ts
+++ libraries/core/src/helpers/log.helper.ts
@@ -3,6 +3,6 @@ import { injectable } from "inversify";
 @injectable()
 export class LogHelper {
   public log(subject: string): void {
-    console.log(`modified 1: ${subject}`);
+    console.log(`modified 2: ${subject}`);
   }
 }
